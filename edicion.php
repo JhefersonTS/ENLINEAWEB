426f4724258a9883a6d7cc38212c690cec97b1c5
Nurymts <JhefersonTS>
2024-10-06 15:14:04 -0500

    primer commit

---
+++ edicion.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>WEB | edicion</title>
+        <?php include 'DEMAS/css.php' ?>
+    </head>
+
+    <body class="px-3">
+
+        <!-- encabezado de la pagina -->
+
+        <?php include 'DEMAS/header.php' ?>
+
+        <!-- cuerpo de la pagina -->
+        <div class="container-md mt-5 mb-5">
+            <div class="card border-0">
+
+                <div class="card-header bg-white border-0">
+                    <h3 class="card-title"><i class="bi bi-plus-circle-fill"></i> EDICIÓN DE LIBROS</h3>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6">
+                            <img src="resources/img/idea_03.PNG" class="w-100" alt="...">
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <p class="card-text" style="text-align: justify;">This is a wider card with supporting text below as a natural lead-in to additional content. 
+                                This content is a little bit longer. This is a wider card with supporting 
+                                text below as a natural lead-in to additional content This is a wider card with supporting text below as a 
+                                natural lead-in to additional content This is a wider card with supporting text below as a natural lead-in to additional 
+                                content This is a wider card with supporting text below as a natural lead-in to additional content This is a wider card with 
+                                supporting text below as a natural lead-in to additional content This is a wider card with supporting text below as a natural lead-in 
+                                to additional content This is a wider card with supporting text below as a natural lead-in to additional content This is a wider card with supporting text below 
+                                as a natural lead-in to additional content This is a wider card with supporting text below as a natural lead-in to 
+                                additional content, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel maxime deleniti mollitia omnis expedita architecto ab tempore excepturi, 
+                                ex cupiditate blanditiis quam, maiores, alias dolores exercitationem aliquid vero debitis temporibus!, Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Amet ratione impedit quia consequuntur obcaecati fugiat quos consectetur sit, laborum corrupti debitis maiores odit cupiditate sunt veritatis, exercitationem quae necessitatibus mollitia.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+
+
+        <!-- pie de la pagina -->
+        <?php include 'DEMAS/footer.php' ?>
+
+    </body>
+
+    </html>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB| inicio</title>
+    <?php include 'DEMAS/css.php' ?>
+</head>
+
+<body class="px-3">
+
+    <!-- encabezado de la pagina -->
+    <?php include 'DEMAS/header.php' ?>
+
+    <!-- cuerpo de la pagina -->
+    <div class="container mt-4">
+
+
+        <h2 class="h2">SOLUCIONES CREATIVAS</h2>
+        <p class="text-justify">sin inportar cual se a tu negocio, nuestro equipon puede hacer todo desde cero.</p>
+
+
+
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img src="resources/img/idea_01.png" class="w-100" alt="">
+                        <h5 class=" h5 mt-4 "><i class="bi bi-pen-fill"></i> DISEÑO</h5>
+                        <p>Creamos tus diseños segun tus necesidades.</p>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img src="resources/img/idea_01.png" class="w-100" alt="">
+                        <h5 class=" h5 mt-4 "><i class="bi bi-flower1"></i> DESARROLLO</h5>
+                        <p>Nuestros progamadores te garantizaran la perfección.</p>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img src="resources/img/idea_01.png" class="w-100" alt="">
+                        <h5 class=" h5 mt-4 "><i class="bi bi-plus-circle-fill"></i> EDICIÓN DE LIBROS</h5>
+                        <p>Que el producto final llegue a tus clentes.</p>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <!-- pie de la pagina -->
+    <?php include 'DEMAS/footer.php' ?>
+
+</body>
+
+</html>
